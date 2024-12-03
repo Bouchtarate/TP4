89c1f635189e02f73bacd21aaf1a34811ceabe2f
Bouchtarate <hamza.bouchtarate@gmail.com>
2024-12-03 13:50:39 +0100

    add routing system to the tp

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import "./main.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Products from "./Produit/products";
-import productData from "./Produit/productData";
-export default function Main() {
+import ProductDetails from "./Produit/productDetails";
+
+function Main() {
   return (
-    <header>
-      <Home />
-      <Products />
-    </header>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/products">Products</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
+    </div>
   );
 }
+
+export default Main;
